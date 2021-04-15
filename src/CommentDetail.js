cd7c3b93a75a6c1cfc4f837947bf1a1e5704392b
Maduri Vassaramo <maduri@vinterior.co>
2021-04-15 19:33:31 +0200

    allow CommentDetails to use props for content rather than hardcoding text

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,21 +1,19 @@
 import React from 'react';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
   return(
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={""}/>
+        <img alt="avatar" src={props.avatar}/>
       </a>
       <div className="content">
-        <div className="meta-data">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <span className="date">Today at 6:00PM</span>
+        <a href="/" className="author">
+          {props.author}
+        </a>
+        <div className="metadata">
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">
-          Nice blog post!
-        </div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   )
